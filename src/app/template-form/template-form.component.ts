9ea714acf92a213ec9c423c08e87d11b27ad3801
Naser Afsar <chita@Nasers-MacBook-Pro.local>
2020-05-04 21:01:55 -0600

    materail API

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,11 +7,41 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
-   
-     name= new FormControl('', [Validators.required, Validators.min(4), Validators.pattern('[a-zA-Z]*')])
+
+
+  userNamePattern = "[a-zA-Z]*";
+  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  jobs = ['', 'Consultant', '', 'Engineer', 'Student',
+    '', 'Recruiter'];
+  submitted = false;
+  title = 'name';
+  emailAddress = 'Email Address';
+  text = 'Message';
+  jobRes = ''; titleRes = ''; textRes = '';
+  model = new Data('', this.jobs[0], '');
+
+
+
+  myname = new FormControl('', [Validators.required, Validators.min(4), Validators.pattern('[a-zA-Z]*')])
+
+  onSubmit(result) {
+    this.titleRes = result.name;
+    this.jobRes = result.myJobs;
+    this.textRes = result.myEmail;
+    this.submitted = true;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+}
+export class Data {
+  constructor(
+    public name?: string,
+    public myJobs?: string,
+    public myEmail?: string
+  ) { }
 
 }
